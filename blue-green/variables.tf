e2ce9452b633b1ba87c577a13f2aa5b754590679
Calvine Otieno <nyarangaotieno@gmail.com>
2022-01-27 13:17:59 +0300

    chore: add variables for region and profile
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

---
+++ blue-green/variables.tf
@@ -0,0 +1,10 @@
+variable "aws_region" {
+  description = "Region in which AWS resources to be created"
+  type = string
+  default = "eu-west-1"
+}
+
+variable "profile" {
+  description = "AWS Credentials Profile configured on your local desktop terminal  $HOME/.aws/credentials"
+  default = "personal-deployment" 
+}
