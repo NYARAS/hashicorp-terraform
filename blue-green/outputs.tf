39e2e6b4e30205582157ca91895e4eaeea59f56f
Calvine Otieno <nyarangaotieno@gmail.com>
2022-01-28 14:17:30 +0300

    feat: add load balancer
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- blue-green/outputs.tf
+++ blue-green/outputs.tf
@@ -2,3 +2,8 @@ output "instance_public_ips" {
   description = "EC2 Instance Public IPs"
   value       = aws_instance.blue-green-deployment.*.public_ip
 }
+
+output "load_balancer_dns" {
+  description = "ELB DNS name"
+  value       = aws_elb.blue-green-deployment.dns_name
+}
