9a708f5eb0f509decc49fc33d8ddd76b42148a00
Calvine Otieno <nyarangaotieno@gmail.com>
2022-01-30 20:56:58 +0300

    feat: add security groups
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

---
+++ blue-green-canary/route_table.tf
@@ -0,0 +1,20 @@
+resource "aws_route_table" "blue-green-deployment" {
+  vpc_id = aws_vpc.blue-green-deployment.id
+
+  route {
+    //associated subnet can reach everywhere
+    cidr_block = "0.0.0.0/0"
+    //CRT uses this IGW to reach internet
+    gateway_id = aws_internet_gateway.blue-green-deployment.id
+  }
+
+  tags = {
+    Name = "Blue/Green Deployment (v${var.infrastructure_version})"
+  }
+}
+
+resource "aws_route_table_association" "blue-green-deployment" {
+  count          = 3
+  subnet_id      = element(local.subnets, count.index)
+  route_table_id = aws_route_table.blue-green-deployment.id
+}
