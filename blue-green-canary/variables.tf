3c786dd2c80a36d1aa116a4bd1e4124248480b76
Calvine Otieno <nyarangaotieno@gmail.com>
2022-01-31 18:25:37 +0300

    chore: add green environment variables
    Signed-off-by: Calvine Otieno <nyarangaotieno@gmail.com>

--- blue-green-canary/variables.tf
+++ blue-green-canary/variables.tf
@@ -30,3 +30,26 @@ variable "instance_type" {
   type        = string
   default     = "t2.micro"
 }
+
+variable "enable_green_env" {
+  description = "Enable green environment"
+  type        = bool
+  default     = false
+}
+
+variable "green_instance_count" {
+  description = "Number of instances in green environment"
+  type        = number
+  default     = 2
+}
+
+variable "traffic_distribution" {
+  description = "Levels of traffic distribution"
+  type        = string
+}
+
+variable "domain_name" {
+  description = "Route53 Hosted domain name"
+  type = string
+  default = "yourdomain.com"
+}
